fabbc9a605bb2d6a51e8611e84a455da83adcf8b
Samuel Salinas <samuel.salinas@one-projects.com>
2021-12-22 22:25:33 +0100

    Fix warnings with va_start

--- src/drawtext.h
+++ src/drawtext.h
@@ -5,27 +5,30 @@
 #include <SDL2/SDL_ttf.h>
 #include <string>
 
-#define BUFFER_SIZE 256
-#define INITIAL_CHARACTER 32
-#define FINAL_CHARACTER 255
-#define TOTAL_CHARACTERS FINAL_CHARACTER - INITIAL_CHARACTER
+class DrawText {
+public:
+  DrawText(const char *fontPath, int fontSize, const SDL_Color &fontColor);
+  ~DrawText();
 
-class DrawText
-{
-    private:
-            SDL_Surface *alphabet[TOTAL_CHARACTERS];
-            bool error;
-            bool newLine;
-            
-            void createAlphabet(TTF_Font *font, const SDL_Color &color);
-            void drawGlyph(SDL_Surface *dst, const unsigned char c, int &x, int &y);
-            void drawGlyphW(SDL_Surface *dst, const wchar_t c, int &x, int &y);
-    public:
-            DrawText(const char *pathFont, const int size, const SDL_Color &color);
-            ~DrawText();
-            
-            void print(SDL_Surface *dst, const char *str, const int x, const int y, ...);                        
-            void print(SDL_Surface *dst, const wchar_t * str, const int x, const int y, ...);
+  void print(SDL_Surface *destinationSurface, const char *string, int x, int y,
+             ...);
+  void print(SDL_Surface *destinationSurface, const wchar_t *string, int x,
+             int y, ...);
+
+private:
+  void createAlphabet(TTF_Font *font, const SDL_Color &fontColor);
+  void drawGlyph(SDL_Surface *destinationSurface, unsigned char character,
+                 int &x, int &y);
+  void drawGlyphW(SDL_Surface *destinationSurface, wchar_t character, int &x,
+                  int &y);
+
+  static constexpr int BUFFER_SIZE = 256;
+  static constexpr int INITIAL_CHARACTER = 32;
+  static constexpr int FINAL_CHARACTER = 255;
+  static constexpr int TOTAL_CHARACTERS = FINAL_CHARACTER - INITIAL_CHARACTER;
+
+  SDL_Surface *alphabet[TOTAL_CHARACTERS]{};
+  bool newLine{false};
 };
 
 #endif
